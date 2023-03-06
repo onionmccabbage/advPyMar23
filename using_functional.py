76dab6e95531a033a944d7e464fbfa37cdc63970
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-06 10:59:47 +0000

    just before coffee day 1

---
+++ using_functional.py
@@ -0,0 +1,16 @@
+# here we explore map, filter etc.
+
+def squareIt(x):
+    '''return the square of the argument'''
+    return x*x # or x**2
+
+def addThem(x,y):
+    '''return the rum of the arguments'''
+    return x+y
+
+def isOdd(n):
+    '''return True or False depending if the argument is odd or not'''
+    return n%2 != 0 # True if odd, False if even
+
+if __name__ == '__main__':
+    pass
