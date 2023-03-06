96af1eb6ec480ec37a144defcc7a9bdf40573e8a
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-06 16:25:10 +0000

    more DB code

--- using_db/read_db.py
+++ using_db/read_db.py
@@ -1,11 +1,14 @@
+# for most purposes th SQL is very similar across different Database implementations
 import sqlite3
 
 def readDB():
-    ''' read back fro mthe database '''
+    ''' read back from the database '''
     conn = sqlite3.connect('mydb')
     curs = conn.cursor()
+    # while SELECT * is allowed, SELECT n, m, p is preferrable - more obvious
+    # SELECT will return members IN ORDER of SELECTION
     st = '''
-    SELECT * FROM zoo
+    SELECT count, cost, creature FROM zoo
     '''
     try:
         curs.execute(st)
@@ -19,7 +22,8 @@ def readDB():
     # decide what to return
     animal_l = []
     for animal in rows: # iterate over the rows of returned data
-        animal_l.append('we have {1} {0} each costing {2:0.2f}'.format(animal[0], animal[1], animal[2]))
+        # remember - we asked for count then cost the ncreature in that order
+        animal_l.append('we have {0} {2} each costing {1:0.2f}'.format(animal[0], animal[1], animal[2]))
     return animal_l # we must return the completed list
 
 if __name__ == '__main__':
