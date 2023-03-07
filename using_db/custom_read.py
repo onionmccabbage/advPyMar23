9ce6d9726a59a0ae6ca2d2f310e9a4bbd425e62c
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-07 10:43:28 +0000

    db corrected

--- using_db/custom_read.py
+++ using_db/custom_read.py
@@ -1,4 +1,5 @@
-import db_util
+import db_util as db # we would then use db.conn and db.curs
+# from db_util import conn, curs # then we just use conn and curs
 
 def custom_readDB():
     '''Ask the user which creature they would like to read from the DB'''
@@ -17,12 +18,12 @@ def custom_readDB():
     # careful - suppose someone wrote malicious code - we MUST validate
     # now we execute our statement against our database access object
     try:
-        db_util.curs.execute(st)
-        rows = db_util.curs.fetchall()
+        db.curs.execute(st)
+        rows = db.curs.fetchall()
     except Exception as err:
         print(err)
     finally:
-        db_util.conn.close()
+        db.conn.close()
     # iterate over the returned data (we expect ONE member)
     for animal in rows:
         print(f'We have {animal[1]} {animal[0]} each costing {animal[2]}')
--- using_db/db_util.py
+++ using_db/db_util.py
@@ -1,5 +1,5 @@
 # here we carry out the generic DB steps we need
 import sqlite3
 
-conn = sqlite3.connect('db')
+conn = sqlite3.connect('mydb')
 curs = conn.cursor()
