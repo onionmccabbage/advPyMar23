65b756cd09bb31a8f10cf10039f40b926d78b0c0
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-08 12:16:27 +0000

    final unittest example

--- using_tests/test_my_point.py
+++ using_tests/test_my_point.py
@@ -6,7 +6,7 @@ class testPoint(unittest.TestCase): # convention
     def setUp(self): # this will run BEFORE every test
         super().setUp() # call the parent setUp
         # define an instance of a Point to be tested
-        self.point = Point(3, 5)
+        self.point = Point(3, 5) # since each test gets a fresh point, they remain independent
     # here are the test cases
     def testMoveByXY(self):
         '''test we can move a point by x and y'''
@@ -14,13 +14,17 @@ class testPoint(unittest.TestCase): # convention
         self.assertEqual( self.point.display(), (8, 7) )
     def testMoveByX(self):
         '''test we can move a point by x'''
-        self.point.moveBy(5)
+        self.point.moveBy(5) # pass x as a positional argument
         self.assertEqual( self.point.display(), (8, 5) )
     def testMoveByY(self):
         '''test we can move a point by y'''
-        self.point.moveBy(y=5)
+        self.point.moveBy(dy=5) # pass y as a keyword argument
         self.assertEqual( self.point.display(), (3, 10) )
-
+    def testHypot(self):
+        '''test that the hypotenuse is derived as expected'''
+        self.point.moveBy(dy=-1) # point is now (3, 4)
+        h = self.point.hypot()
+        self.assertEqual(h, 5.00)
 
 if __name__ == '__main__':
     unittest.main() # always call the tests like this
