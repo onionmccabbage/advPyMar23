dbfdba87898beb2baca6eb5d153dd972e896b79a
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-08 12:24:24 +0000

    final doctest example

--- using_tests/using_doctest.py
+++ using_tests/using_doctest.py
@@ -10,7 +10,19 @@ def showSquare(n):
     '''
     return n*n
 
+def showCubes(a, b):
+    '''return the cube of all numbers between a and b
+    >>> showCubes(0, 3)
+    [0, 1, 8]
+    >>> showCubes(0, 101) # doctest:+ELLIPSIS
+    [0, 1, 8, ..., 1000000]
+    '''
+    answers = []
+    for i in range(a,b):
+        answers.append( i**3 )
+    return answers
 
 if __name__ == '__main__':
     # print( showSquare(3) ) # 9
+    # print(showCubes(0, 9))
     doctest.testmod(verbose=True)
