62ff027a4bb91d9723989d5ccd979cccf033297a
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-09 10:53:02 +0000

    example series and parallel function

--- using_threads/fn_thread_eg.py
+++ using_threads/fn_thread_eg.py
@@ -15,7 +15,7 @@ def myfunc(name):
 if __name__ == '__main__':
     # remember - this will use ONE processor with MUTLIPLE threads
     start = timeit.default_timer()
-    # run the function on several threads
+    # run the function on several threads (i.e. run them in parallel)
     thread1 = Thread( target=myfunc, args=('t1',) )
     thread1.start() # this gets our new thad running
     thread2 = Thread( target=myfunc, args=('t2',) )
@@ -25,5 +25,10 @@ if __name__ == '__main__':
     thread1.join() # this is good practice - tell the thread to rejoin the main thread
     thread2.join()
     thread3.join()
+
+    # we can try running the same thing in sequence...
+    # myfunc('f1') # these three functions run on the MAIN thread
+    # myfunc('f2') # this line will only run when the previous line has completed
+    # myfunc('f3') # we call this 'blocking'
     end = timeit.default_timer() # careful - this time the main thread only!!
     print('', f'total time {end-start}') # 2.5 sec in total (2.5 sec both in parallel)
